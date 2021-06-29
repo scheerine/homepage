924d1774511e9c635fe6532770af63cd6a92c70d
PhilippMatthes <github@philippmatth.es>
2021-06-29 15:18:33 +0200

    Add basic ink header

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-$771y+7j9(8!_=x!#a1&ptv%gb+l)y$mvdf7x9kjfwv9xt&6^6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'homepage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,11 +114,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+CACHE_CONTROL_MAX_AGE = 7 * 24 * 3600
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected')
+
+# Media Files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- homepage/homepage/urls.py
+++ homepage/homepage/urls.py
@@ -15,7 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.decorators.cache import cache_control
+from django.contrib.staticfiles.views import serve
+
+
+from . import views as homepage_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', homepage_views.LandingView.as_view(), name='landing')
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        view=cache_control(max_age=settings.CACHE_CONTROL_MAX_AGE)(serve),
+        document_root=settings.STATIC_ROOT
+    )

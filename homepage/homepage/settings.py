41615f6e60b744c27152c0a6c4c4029eaf60640a
PhilippMatthes <github@philippmatth.es>
2021-09-10 14:45:23 +0200

    Update website

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -104,7 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'de'
+
+LANGUAGES = [
+    ('de', 'German'),
+]
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 TIME_ZONE = 'UTC'
 

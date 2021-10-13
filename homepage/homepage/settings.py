3123d7d1e5764f6573fb762d54a2c7093cc29ad7
PhilippMatthes <github@philippmatth.es>
2021-10-13 11:35:09 +0200

    Add whitenoise to installed apps

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -39,10 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'huey.contrib.djhuey',
-
     'entries',
     'gitload',
+
+    'huey.contrib.djhuey',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
